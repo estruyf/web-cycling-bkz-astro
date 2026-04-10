01f4a28cb7348625d56313c9419eb1de310ffee1
Elio Struyf <elio@struyfconsulting.be>
2026-04-10 16:19:19 +0200

    Add WeeklyRidesSection component and implement ride filtering; update global styles for button hover

---
+++ src/models/Club.ts
@@ -0,0 +1,33 @@
+export interface Ride {
+  day:
+    | "Monday"
+    | "Tuesday"
+    | "Wednesday"
+    | "Thursday"
+    | "Friday"
+    | "Saturday"
+    | "Sunday";
+  time: string;
+  type: string;
+  notes?: string;
+  openForAll: boolean;
+}
+
+export interface ClubData {
+  name: string;
+  town: "Beveren" | "Kruibeke" | "Zwijndrecht" | "Melsele";
+  shortDescription: string;
+  logo?: string;
+  website?: string;
+  meetingPoint: string;
+  meetingPointDetail?: string;
+  contactEmail?: string;
+  active: boolean;
+  rides: Ride[];
+}
+
+export interface Club {
+  id: string;
+  body: string;
+  data: ClubData;
+}
---
+++ src/models/WeekOverzicht.ts
@@ -0,0 +1,9 @@
+export interface WeekritOverzicht {
+  clubNaam: string;
+  clubSlug: string;
+  dag: string;
+  uur: string;
+  type: string;
+  vertrekpunt: string;
+  openVoorIedereen: boolean;
+}
